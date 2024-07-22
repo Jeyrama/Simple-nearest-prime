a9bb7f0ac0243accd20a0a55cf61fe76ecaec177
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-07-21 18:03:08 -0700

    Create Simple-nearest-prime.js

---
+++ Simple-nearest-prime.js
@@ -0,0 +1,7 @@
+/*
+In this challenge, you will be given a number and 
+your task will be to return the nearest prime number.
+
+solve(4) = 3. The nearest primes are 3 and 5. If difference is equal, pick the lower one. 
+solve(125) = 127
+*/
